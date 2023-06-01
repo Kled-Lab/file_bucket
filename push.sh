13e34000a9621f9b921a7ef71e5d9d53ddbe14de
yuanl <yuanlbox@gmail.com>
2023-06-01 10:53:45 +0800

    添加图片

--- push.sh
+++ push.sh
@@ -3,5 +3,8 @@
 cd /Users/kled/Code/bucket/file_bucket
 git add .
 git commit -m'添加图片'
+
+export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+
 git push origin main
 echo "上传完成😄!"
