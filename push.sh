ea003f155de86027f0054a741ec2dec606ef2746
yuanl <yuanlbox@gmail.com>
2023-06-03 19:14:57 +0800

    1

--- push.sh
+++ push.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
 cd /Users/kled/Code/bucket/file_bucket
-git add .
-git commit -m'添加图片'
+#git add .
+#git commit -m'添加图片'
+#
+#export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+#
+#git push origin main
+#echo "Upload Success: "
 
-export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
 
-git push origin main
-echo "上传完成😄!"
+changelog=`git status --untracked-files`
+
+echo $changelog > changelog.txt
+
+targetlog=`ggrep -Pio '(?=blog).*?(?= )' ./changelog.txt`
+
+echo $targetlog > targetlog.txt
+
+rm -rf ./changelog.txt
+
+#reg='/(?<=include).*?(?=changes)'
+#echo current_git_branch_latest_id | grep -Eo reg
+
+#echo current git branch latest commit id=$current_git_branch_latest_id
